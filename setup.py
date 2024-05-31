66a771474cfce8fe745dad9917ec3e4a327514eb
pbj8723 <pjh8723@gmail.com>
2024-05-31 22:07:25 +0900

    Create environment

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='teemolab',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Teemolab to analyze the data of league of legends',
+    author='Teemo',
+    license='',
+    url='https://github.com/pbj8723/Teemo-Lab',
+)
