5c705171c7f24beb27235b7bee985b02de87bd79
pbj8723 <pjh8723@gmail.com>
2024-08-29 13:00:25 +0900

    lint

--- tests/utils/test_core.py
+++ tests/utils/test_core.py
@@ -4,8 +4,7 @@ from teemolab.utils import core
 
 
 def test_safe_division():
-
     assert core.safe_division(1, 2) == pytest.approx(0.5)
-    assert core.safe_division(1, 0) == None
+    assert core.safe_division(1, 0) is None
     assert core.safe_division(0, 1) == pytest.approx(0.0)
-    assert core.safe_division(0, 0) == None
+    assert core.safe_division(0, 0) is None
